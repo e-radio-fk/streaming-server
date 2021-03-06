fca3da4904816635a50a439d0519ad9e9d711026
npyl <ulcheats@gmail.com>
2021-03-07 01:56:36 +0200

    code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// hello
+const io = require("socket.io-client")(8080, {
+    path: "/stream",
+});
+
+io.on("connection", (socket) => {
+    socket.emit("hi", "everyone");
+});
+
+io.on("disconnect", (reason) => {
+    console.log(reason);
+});
