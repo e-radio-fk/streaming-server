abbaf60e077b58971c2da048522b9d6c0897f39c
npyl <ulcheats@gmail.com>
2021-04-02 17:41:35 +0300

    WIP: migrate site to DigitalOcean and Node.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // TODO: do not allow every origin! see CORS.
 
-const io = require("socket.io")(8081, {
-	cors: {
-		origin: '*',
-	}
+import { createServer, staticProvider } from 'express';
+
+var app = createServer();
+
+app.use(staticProvider(__dirname + '/public'));
+
+app.all('*', function(req, res) {
+  res.sendFile('index.html');
 });
 
-io.on("connection", (socket) => {
-	socket.emit("hello", "world");
-});
+// const io = require("socket.io")(8081, {
+// 	cors: {
+// 		origin: '*',
+// 	}
+// });
+
+// io.on("connection", (socket) => {
+// 	socket.emit("hello", "world");
+// });
