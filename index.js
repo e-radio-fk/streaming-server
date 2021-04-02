5b008b168b0d34bf43b48d86ba83e683d4ecad5d
npyl <ulcheats@gmail.com>
2021-04-02 17:29:18 +0300

    server communicates with client

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const io = require("socket.io")(8080, {
-  	path: "/stream",
+// TODO: do not allow every origin! see CORS.
+
+const io = require("socket.io")(8081, {
+	cors: {
+		origin: '*',
+	}
 });
 
 io.on("connection", (socket) => {
