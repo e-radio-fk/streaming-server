427afdd6eafe909f2476a7aa6609e1b2a47da2d4
npyl <ulcheats@gmail.com>
2021-07-11 12:58:33 +0300

    remove obsolete code

--- docs/scripts/login.js
+++ docs/scripts/login.js
@@ -2,54 +2,6 @@
 //  helpers
 //
 
-/* get a copy of the sign_in_up_space node */
-const sign_in_up_space_copy = document.getElementById('sign-in-or-up-space-id').cloneNode(true);
-
-function sign_in_up_box_set_visibility(id, visibility) {
-    document.getElementById(id).style.visibility = visibility;
-    
-    var nodes = document.getElementById(id).children;
-    for(var i = 0; i < nodes.length; i++) {
-        nodes[i].style.visibility = visibility;
-    }
-
-    var inverse_visibility = (visibility == 'visible') ? 'hidden' : 'visible';
-
-    document.getElementById('sign-in-button').style.visibility = inverse_visibility;
-    document.getElementById('sign-in-text').style.visibility = inverse_visibility;
-}
-
-//
-// Sign-in Box
-//
-function show_login_box() {
-    document.getElementById('sign-in-button').remove();
-    document.getElementById('sign-in-text').remove();
-
-    sign_in_up_box_set_visibility('login-box', 'visible');
-}
-function hide_login_box() {
-    document.getElementById('sign-in-or-up-space-id').innerHTML = sign_in_up_space_copy.innerHTML;
-
-    sign_in_up_box_set_visibility('login-box', 'hidden');
-}
-
-//
-// Sign-up Box
-//
-function show_signup_box() {
-    document.getElementById('sign-in-button').remove();
-    document.getElementById('sign-in-text').remove();
-    document.getElementById('login-box').remove();
-
-    sign_in_up_box_set_visibility('signup-box', 'visible');
-}
-function hide_signup_box() {
-    document.getElementById('sign-in-or-up-space-id').innerHTML = sign_in_up_space_copy.innerHTML;
-
-    sign_in_up_box_set_visibility('signup-box', 'hidden');
-}
-
 //
 // Methods
 //
@@ -58,7 +10,6 @@ function sign_in() {
     var email = document.getElementById('login-form-uname').value;
     var passw = document.getElementById('login-form-passw').value;
 
-    // TODO: implement remember me
     // TODO: sanitisation checks
 
     /* set sign-in persistance to be LOCAL: even after the browser closes the user is still logged in! */
