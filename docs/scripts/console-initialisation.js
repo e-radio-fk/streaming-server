592d9ffe87ed4d2fab179ef4e60c386a15773f0a
npyl <ulcheats@gmail.com>
2021-07-12 19:54:33 +0300

    Add date planner

---
+++ docs/scripts/console-initialisation.js
@@ -0,0 +1,40 @@
+/* get current user */
+var user = JSON.parse(sessionStorage.getItem('currentUser'));
+if ((!user) || (user.uid == undefined) || user == 'no-user')
+{
+    console.log('console: restricting view to unauthorised user!');
+
+    window.location.pathname = '/?error=UnauthorisedUser';
+}
+else
+{
+    /* construct photo path */
+    var serverFilePath = '/' + user.uid + '/user_photo';
+    var photoURL = 'https://eradiofk.sirv.com' + serverFilePath;
+    
+    console.log('got photo: ', photoURL);
+    
+    var userPhoto = document.getElementById('banner-user-img');
+    
+    if (photoURL)
+        userPhoto.style.borderRadius = '35%';
+    
+    /* set user photo */
+    userPhoto.src = photoURL;
+    
+    /* initialisation has finished; show the UI */
+    document.getElementsByTagName('header')[0].style.visibility = 'visible';
+    document.getElementsByTagName('body')[0].style.visibility = 'visible';
+
+    /* Initialise MCDatepicker */
+    /* javascript */
+    const datePicker = MCDatepicker.create({
+        el: '#datepicker',
+        bodyType: 'modal'
+    });
+
+    /* hack: on open, quickly change z-index (>= 1061) so that calendar shows up above any bootstrap modal */
+    datePicker.onOpen(() =>{
+        document.getElementsByClassName('mc-calendar--modal')[0].style.zIndex = "1062";
+    });
+}
--- docs/scripts/console.js
+++ docs/scripts/console.js
@@ -1,28 +1,15 @@
-/* get current user */
-var user = JSON.parse(sessionStorage.getItem('currentUser'));
-if ((!user) || (user.uid == undefined) || user == 'no-user')
+/**
+ * schedule_new_podcast()
+ *
+ * Gathers information (such as podcast title, descreption and scheduled time)
+ * from the podcast scheduler modal and uploads it to the Firestore NoSQL database.
+ */
+function schedule_new_podcast() 
 {
-    console.log('console: restricting view to unauthorised user!');
+    var title       = document.getElementById();
+    var description = document.getElementById();
+    var date        = document.getElementById();
+    var time        = document.getElementById();
 
-    window.location.pathname = '/?error=UnauthorisedUser';
-}
-else
-{
-    /* construct photo path */
-    var serverFilePath = '/' + user.uid + '/user_photo';
-    var photoURL = 'https://eradiofk.sirv.com' + serverFilePath;
     
-    console.log('got photo: ', photoURL);
-    
-    var userPhoto = document.getElementById('banner-user-img');
-    
-    if (photoURL)
-        userPhoto.style.borderRadius = '35%';
-    
-    /* set user photo */
-    userPhoto.src = photoURL;
-    
-    /* initialisation has finished; show the UI */
-    document.getElementsByTagName('header')[0].style.visibility = 'visible';
-    document.getElementsByTagName('body')[0].style.visibility = 'visible';
 }
