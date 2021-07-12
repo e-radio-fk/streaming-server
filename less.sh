0232bccf0cac421690ddbca2826517c0377ffa24
npyl <ulcheats@gmail.com>
2021-07-12 17:01:48 +0300

    Add modal support

--- less.sh
+++ less.sh
@@ -2,3 +2,4 @@
 node_modules/lessc/node_modules/less/bin/lessc docs/less/main.less docs/css/main.css
 node_modules/lessc/node_modules/less/bin/lessc docs/less/_dark.less docs/css/_dark.css
 node_modules/lessc/node_modules/less/bin/lessc docs/less/_light.less docs/css/_light.css
+node_modules/lessc/node_modules/less/bin/lessc docs/bootstrap/less/bootstrap.less docs/bootstrap/css/bootstrap.css
