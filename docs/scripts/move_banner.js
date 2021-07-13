aab6a829d7a1dd84d424c248ac9ae824b34e196f
npyl <ulcheats@gmail.com>
2021-07-13 19:40:22 +0300

    Implement getting now-playing song/show from DB

--- docs/scripts/move_banner.js
+++ docs/scripts/move_banner.js
@@ -1,47 +1,61 @@
-var original_string = "Last Christmas I Gave You My Heart";
-
-function get_container_width()
+try
 {
-    // The banner now contains the User Photo; we need to subtract its width!
-    var user_photo_width = 33;  // 33px
-
-    return document.getElementById('now-playing-banner-object').offsetWidth - user_photo_width;
-}
-
-function move_banner()
-{
-    var before = 0;
-    var after = 0;
-    
-    string_off = document.getElementById('now-playing-banner-text').offsetWidth;
-
-    // get old position
-    before = parseInt(document.getElementById('now-playing-banner-text').style.paddingLeft || 0, 10);
-
-    // make sure text will 'cycle'
-    if (string_off + 100 >= get_container_width())
+    var original_string = Cookies.get('now-playing');
+    if (!original_string)
     {
-        var text = document.getElementById('now-playing-banner-text').textContent;
-
-        if (text.length == 0)
-        {
-            before = 0;
-            text = original_string;
-        }
-        else
-            text = text.substr(0, text.length - 2)
-
-        document.getElementById('now-playing-banner-text').textContent = text;
+        console.error('Failed to get the now-playing cookie!');
+        throw new Error('Failed to get the now-playing cookie!');
     }
 
-    // increment position index
-    after = before + 1 + "%";
+    document.getElementById('now-playing-banner-text').innerText = original_string;
 
-    // set new position
-    document.getElementById('now-playing-banner-text').style.paddingLeft = after;
+    function get_container_width()
+    {
+        // The banner now contains the User Photo; we need to subtract its width!
+        var user_photo_width = 33;  // 33px
 
-    // make this a recurring function
-    setTimeout(move_banner, 110);
+        return document.getElementById('now-playing-banner-object').offsetWidth - user_photo_width;
+    }
+
+    function move_banner()
+    {
+        var before = 0;
+        var after = 0;
+        
+        string_off = document.getElementById('now-playing-banner-text').offsetWidth;
+
+        // get old position
+        before = parseInt(document.getElementById('now-playing-banner-text').style.paddingLeft || 0, 10);
+
+        // make sure text will 'cycle'
+        if (string_off + 100 >= get_container_width())
+        {
+            var text = document.getElementById('now-playing-banner-text').textContent;
+
+            if (text.length == 0)
+            {
+                before = 0;
+                text = original_string;
+            }
+            else
+                text = text.substr(0, text.length - 2)
+
+            document.getElementById('now-playing-banner-text').textContent = text;
+        }
+
+        // increment position index
+        after = before + 1 + "%";
+
+        // set new position
+        document.getElementById('now-playing-banner-text').style.paddingLeft = after;
+
+        // make this a recurring function
+        setTimeout(move_banner, 110);
+    }
+
+    move_banner();
 }
-
-move_banner();
+catch (e)
+{
+    console.error('Error getting now-playing: ' + e);
+}
