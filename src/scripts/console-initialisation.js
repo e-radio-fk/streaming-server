ba491be78a9180d9b6ff8bbc36893938f53f93d6
npyl <n.pylarinos@hotmail.com>
2023-05-13 18:07:58 +0300

    Fix starting any podcast

--- src/scripts/console-initialisation.js
+++ src/scripts/console-initialisation.js
@@ -1,28 +1,25 @@
 var database;
 
-function initialise_console_page()
-{
+const initialise_console_page = () => {
     /*
      * Page Initialisation
      */
-    try
-    {
+    try {
         /* construct photo path */
         var serverFilePath = '/' + user.uid + '/user_photo';
         var photoURL = 'https://eradiofk.sirv.com' + serverFilePath;
-        
+
         console.log('got photo: ', photoURL);
-        
+
         var userPhoto = document.getElementById('banner-user-img');
-        
+
         if (photoURL)
             userPhoto.style.borderRadius = '35%';
-        
+
         /* set user photo */
         userPhoto.src = photoURL;
     }
-    catch(error)
-    {
+    catch (error) {
         console.log(error);
     }
 
@@ -30,8 +27,7 @@ function initialise_console_page()
      * Modals Initialisation & Configuration 
      */
 
-    try
-    {
+    try {
         // TODO:
         /* on modal exit, all elements must be reverted to normal */
 
@@ -53,66 +49,55 @@ function initialise_console_page()
             document.getElementById('podcast-date-picked-label').innerHTML = formatedDate;
         });
     }
-    catch(error)
-    {
+    catch (error) {
         console.log(error);
     }
 
     /*
      * Initialise Firebase Realtime Database
      */
-    try
-    {
+    try {
         database = firebase.database();
 
         /*
          * Fill contents of the start-podcast dropdown
          */
-        var start_podcast_dropdown_content = document.getElementById('start-podcast-dropdown-content');
-        if (start_podcast_dropdown_content)
-        {
-            database.ref().child('/scheduled-podcasts').get().then((snapshot) => {
-                if (snapshot.exists())
-                {
-                    function html(podcast_title, podcast_id)
-                    {
-                        var html = "<p class='dropdown-content-button' onclick='start_podcast('" + podcast_id + "')'>" + podcast_title + "</p>"
-                        return html;
-                    }
-    
-                    var inner_html;
-                    var json = snapshot.val();
-    
-                    if (json)
-                    {
-                        Object.keys(json).forEach(key => {
-                            var element = json[key];
-        
-                            if (element)
-                            {
-                                var title      = element['title'];
-                                var podcast_id = element['id'];
-            
-                                /* skip erroneous */
-                                if (title && podcast_id)
-                                    inner_html += html(title, podcast_id);
-                            }
-                        });
-        
-                        start_podcast_dropdown_content.innerHTML += inner_html;
-                    }
+        const start_podcast_dropdown_content = $('#start-podcast-dropdown-content');
+
+        database.ref().child('/scheduled-podcasts').get().then((snapshot) => {
+            if (!snapshot.exists())
+                throw "Failed to get snapshot!";
+
+            const json = snapshot.val();
+            if (!json)
+                throw "Failed to get json";
+
+            Object.keys(json).forEach(key => {
+                const element = json[key];
+
+                if (element) {
+                    var title = element['title'];
+                    var podcast_id = element['id'];
+
+                    /* skip erroneous */
+                    if (title && podcast_id)
+                        start_podcast_dropdown_content.append(
+                            $('<p>')
+                                .text(title)
+                                .addClass('dropdown-content-button')
+                                .click(() => {
+                                    start_podcast(podcast_id);
+                                })
+                        );
                 }
-                else
-                {
-                    // show_error('Δεν έχουν προγραμματιστεί podcast για αυτή τη βδομάδα! Μπορείς να ξεκινήσεις νέο!', '');
-                }
-            }).catch((error) => {
-                console.error('Error getting a list of week\'s scheduled podcasts: ' + error);
             });
-        }
+
+
+        }).catch((error) => {
+            console.error('Error getting a list of week\'s scheduled podcasts: ' + error);
+        });
     }
-    catch(error)
-    {
+    catch (error) {
         console.log(error);
     }
 }
